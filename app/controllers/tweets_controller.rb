991d42493d87d6383e587af4af8a9e9e6d5e6414
grapplermaki <maki_nakashima@ezweb.ne.jp>
2019-01-03 13:56:10 +0900

    アクセストークンを削除、postactionで地の文を投稿できるよう修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,26 +11,19 @@ class TweetsController < ApplicationController
     end
 
     def post
-        #d = Date.today
-        #tweet = Tweet.order("RANDOM()").first
-        #tweet = Tweet.where("SELECT text from tweets where id = '1'" )
-        #tweet = Tweet.where("id = '3'" ).first
-        #tweet = Tweet.where("day = strftime('%m月%d日', 'now')").first
-        tweet = Tweet.where("id = '' ").first
-        #tweet = Tweet.where("day = strftime('%m%d', '2018-12-22')").first
-        #tweet = Tweet.order("SELECT text from tweets where day = strftime('%m月%d日', 'now');").first
+        t = Time.current
+        #tweet = Tweet.where(day: Date.today.strftime("%m%d")).first
+        tweet = Tweet.where(day: t.next_month.strftime("%m%d")).first
         status = tweet.text
-        #media = open(tweet.image)
-        #@client.update_with_media(status, media)
-        #response = @client.update(status)
         response = @client.update(status)
-        Rails.logger.debug response
+        response = @client.update("#{status} と地の文")
+        #Rails.logger.debug response
         redirect_to :root
     end
     
     def twitter_client
         @client = Twitter::REST::Client.new do |config|
-          config.consumer_key = ENV['CONSUMER_KEY'] 
+          config.consumer_key = ENV['CONSUMER_KEY']
           config.consumer_secret = ENV['CONSUMER_SECRET']
           config.access_token = ENV['ACCESS_TOKEN']
           config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
