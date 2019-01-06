82b596653a218e5e51305bcd03d76618a2a82123
grapplermaki <maki_nakashima@ezweb.ne.jp>
2019-01-06 17:33:28 +0900

    明日、一週間後、一ヶ月後を呟けるように

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,10 +13,15 @@ class TweetsController < ApplicationController
     def post
         t = Time.current
         #tweet = Tweet.where(day: Date.today.strftime("%m%d")).first
-        tweet = Tweet.where(day: t.next_month.strftime("%m%d")).first
+        tweet = Tweet.where(day: t.tomorrow.strftime("%m%d")).first
+        tweet2 = Tweet.where(day: t.since(7.days).strftime("%m%d")).first
+        tweet3 = Tweet.where(day: t.next_month.strftime("%m%d")).first
         status = tweet.text
-        response = @client.update(status)
-        response = @client.update("#{status} と地の文")
+        status2 = tweet2.text
+        status3 = tweet3.text
+        #response = @client.update(status)
+        response = @client.update("明日#{t.tomorrow.strftime("%m")}月#{t.tomorrow.strftime("%d")}日は　#{status}です。\r1週間後#{t.since(7.days).strftime("%m")}月#{t.since(7.days).strftime("%d")}日は　#{status2}です。\r1ヶ月後#{t.next_month.strftime("%m")}月#{t.next_month.strftime("%d")}日は　#{status3} です。 ")
+        #response = @client.update("#{status} と地の文")
         #Rails.logger.debug response
         redirect_to :root
     end
